2a8485423ae273c30fe881a816e0299ac0bf8499
1107473010@qq.com <1107473010@qq.com>
2024-07-16 21:36:13 +0800

    🐞 fix(all): 修复异常文件。

--- src/file/ch01-03/test.sh
+++ src/file/ch01-03/test.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 cat > test.txt << EOF
@@ -17,24 +16,4 @@ for ((i=2;i<=$num;i++))
 do
     let fibo[$i]=fibo[$i-1]+fibo[$i-2]
 done
-=======
-#!/bin/bash
-
-cat > test.txt << EOF
-这是测试文件!
-EOF
-
-[ -f "err.log" ] && echo Y || echo N
-[ "err.log" -ef "err2.log" ] && echo Y || echo N
-[ "err.log" -nt "err2.log" ] && echo Y || echo N
-[ "err.log" -ot "err2.log" ] && echo Y || echo N
-[ "err.log" -ot "errNil.log" ] && echo Y || echo N
-
-num=12
-fibo=(1 1)
-for ((i=2;i<=$num;i++))
-do
-    let fibo[$i]=fibo[$i-1]+fibo[$i-2]
-done
->>>>>>> 8fbadba8171e202b4bb61f4163dc24f60bafc2a6
 echo ${fibo[@]}
